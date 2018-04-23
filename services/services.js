06a69ae26a8d5c250555454af882d58488971d36
cone-7 <cone-7@hotmail.com>
2018-04-23 11:17:29 -0500

    Remove critical info

--- services/services.js
+++ services/services.js
@@ -1,9 +1,6 @@
 export function infoUserService(userGithub){
 	return fetch('https://api.github.com/users/'+userGithub+'/repos', {
 		method: 'GET',
-		headers: new Headers({
-			'Authorization': 'token f18ce718e3dc8564b4b80b97d94323368a0851e0', 
-   		}), 
 	}).then((response) => {
 		return response.json();
 	});
@@ -12,9 +9,6 @@ export function infoUserService(userGithub){
 export function issuesFromRepoService(userGithub, repo){
 	return fetch('https://api.github.com/repos/'+userGithub+'/'+repo+'/issues', {
 		method: 'GET',
-		headers: new Headers({
-			'Authorization': 'token f18ce718e3dc8564b4b80b97d94323368a0851e0', 
-   		}),
 	}).then((response) => {
 		return response.json();
 	});
